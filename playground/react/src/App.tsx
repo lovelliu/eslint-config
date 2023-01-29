32333fd3385e172a3e5599d6f4112faa9c2a15dc
Lovell Liu <lovelliu@foxmail.com>
2023-01-29 14:37:41 +0800

    feat: add some eslint rules for react

--- playground/react/src/App.tsx
+++ playground/react/src/App.tsx
@@ -5,18 +5,24 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div className="App">
+    <div
+      className="App"
+    >
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={ reactLogo } className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button onClick={ () => setCount(count => count + 1) }>
           count is {count}
         </button>
         <p>
